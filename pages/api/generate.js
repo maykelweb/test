56df6fc70d08eb8b4af2baf02fea5530cb5ed2f9
Maykel Delgado <maykeld@outlook.com>
2023-01-13 17:41:36 +0000

    added extra functionality, changed looks

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -27,10 +27,10 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      //model: "text-ada-001",
+      //model: "text-davinci-003",
+      model: "text-ada-001",
       prompt: generatePrompt(chat),
-      temperature: 0.1,
+      temperature: 0.6,
       max_tokens: 500, 
     });
     res.status(200).json({ result: completion.data.choices[0].text });
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,17 @@ export default function Home() {
 
   async function onSubmit(event) {
     event.preventDefault();
+
+    const chatbox = document.querySelector("#chatbox");
+
+    const div = document.createElement("div");
+    div.className = styles.user__messages;
+    div.innerHTML = `<p><span>You:</span> ${chatInput}</p>`;
+    chatbox.appendChild(div);
+    chatbox.scrollTop = chatbox.scrollHeight;
+    
+    setChatInput("");
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -16,14 +27,23 @@ export default function Home() {
         },
         body: JSON.stringify({ chat: chatInput }),
       });
-
+      
+      setChatInput("");
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
       setResult(data.result);
-      setChatInput("");
+
+      // append message as div to chatbox
+      const chatbox = document.querySelector("#chatbox");
+      const div = document.createElement("div");
+      div.className = styles.healthchat__messages;
+      div.innerHTML = `<p><span>Health Chat:</span> ${data.result}</p>`;
+      chatbox.appendChild(div);
+      chatbox.scrollTop = chatbox.scrollHeight;
+
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -35,25 +55,43 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Health Chat</title>
-        <link rel="icon" href="/health-chat-logo.png" />
+        <link rel="icon" href="/health-chat-logo.jpg" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/health-chat-logo.png" className={styles.icon} />
-        <h3>Health Chat</h3>
+        <div className={styles.chat}>
+          <h3>Health Chat</h3>
         
-        <div className={styles.result}>{result}</div>
+          <div id="chatbox" className={styles.chatbox}>
+            <div className={styles.healthchat__messages}>
+              <p><span>Health Chat:</span> You are now talking to Healthchat, here to answer all your healthcare needs. Just so you know, I am not a licensed doctor, but I do use the latest NHS information, and can also give guidance on natural remedies and nutritional information that could help your health if asked.</p>
+            </div>
+          </div>
         
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="chat"
-            placeholder="Enter your symptoms"
-            value={chatInput}
-            onChange={(e) => setChatInput(e.target.value)}
-          />
-          <input type="submit" value="Chat" />
-        </form>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="chat"
+              placeholder="Tell health chat about your health"
+              value={chatInput}
+              onChange={(e) => setChatInput(e.target.value)}
+            />
+            <input type="submit" value="Send Message" />
+          </form>
+        </div>
+        <div className={styles.about}>
+          <img src="/health-chat-logo.jpg" className={styles.icon} />
+          <p> The most advanced Ai healthcare assistant that can give you an instant diagnosis and treatment plan based on your symptoms. </p>
+          <p>
+          Take the guesswork out of healthcare! Get the best diagnosis and treatment advice from trusted sources based on your input.
+          </p>
+
+          <div>
+            <img src="/the-bag.jpg" className={styles.partners} />
+            <img src="/harley-street.jpg" className={styles.partners} />
+            <img src="/dtm-global.jpg" className={styles.partners} />
+          </div>
+        </div>
       </main>
     </div>
   );
